b98611c5f4f27f77e38b8a59816c15deff44621c
Sebastian Yeo <khyeo81@gmail.com>
2026-03-16 22:51:58 -0700

    sudo fix

--- internal/steam/client_linux.go
+++ internal/steam/client_linux.go
@@ -11,9 +11,14 @@ import (
 
 func setTAPIP(ip uint32, device *tap.Device) error {
 	ipString := fmt.Sprintf("%d.%d.%d.%d", ip>>24, (ip>>16)&0xFF, (ip>>8)&0xFF, ip&0xFF)
-	ipCmd := exec.Command("ip", "addr", "add", fmt.Sprintf("%s/24", ipString), "dev", device.Name())
+	ipCmd := exec.Command("sudo", "ip", "addr", "add", fmt.Sprintf("%s/24", ipString), "dev", device.Name())
 	if err := ipCmd.Run(); err != nil {
 		log.Printf("Failed to set ip address to %s", ipString)
+		return err
+	}
+	upCmd := exec.Command("sudo", "ip", "link", "set", "dev", device.Name(), "up")
+	if err := upCmd.Run(); err != nil {
+		log.Printf("Failed to set interface %s up", device.Name())
 		return err
 	}
 	return nil
